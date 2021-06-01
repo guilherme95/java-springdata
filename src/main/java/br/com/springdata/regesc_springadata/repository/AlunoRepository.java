b6a803e1c27a493c489819bcbf0150d904d9c2cc
Guilherme Victor de Mello <guilherme.mello@AVMB.COM.BR>
2021-06-01 09:58:53 -0300

    implemented new queries sqls and jpql on study project

--- src/main/java/br/com/springdata/regesc_springadata/repository/AlunoRepository.java
+++ src/main/java/br/com/springdata/regesc_springadata/repository/AlunoRepository.java
@@ -1,6 +1,7 @@
 package br.com.springdata.regesc_springadata.repository;
 
 import br.com.springdata.regesc_springadata.orm.Aluno;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +9,12 @@ import java.util.List;
 public interface AlunoRepository extends CrudRepository<Aluno, Long> {
     List<Aluno> findByNomeContaining(String nome);
     List<Aluno> findByNomeContainingAndIdadeLessThan(String nome, Integer idade);
+
+    // JPQL
+    @Query("SELECT a FROM Aluno a WHERE a.nome LIKE :nome% AND a.idade >= :idade")
+    List<Aluno> findNomeIdadeMaiorOuIgual(String nome, Integer idade);
+
+    // JPQL
+    @Query("SELECT a FROM Aluno a INNER JOIN a.disciplinas d WHERE a.nome LIKE :nomeAluno% AND a.idade >= :idadeAluno AND d.nome LIKE :nomeDisciplina%")
+    List<Aluno> findNomeIdadeIgualOuMaiorMatriculado(String nomeAluno, Integer idadeAluno, String nomeDisciplina);
 }
